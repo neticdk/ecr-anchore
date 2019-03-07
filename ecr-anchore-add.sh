b6f4da3fd4f8fb48258c3492224e75d02ba0fd27
John Cattrall <johncattrall@gmail.com>
2019-03-07 21:41:52 +0700

    tidying up

--- ecr-anchore-add.sh
+++ ecr-anchore-add.sh
@@ -8,11 +8,16 @@ region=
 ecrid=
 
 repolist=$(
-    aws ecr describe-repositories --profile $profile | grep repositoryName | awk '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' | awk '{ print "aws ecr describe-images --repository-name " $1 }' | sed 's/.* //g')
+    aws ecr describe-repositories --profile $profile | grep repositoryName \
+        | awk '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' \
+        | awk '{ print "aws ecr describe-images --repository-name " $1 }' | sed 's/.* //g')
 
 for repo in $repolist
 do
-    aws ecr describe-images --profile=$profile --repository-name $repo --output json | jq '.imageDetails[] | [.repositoryName, .imageTags[]] | @csv' | sed 's/"\"//g' | sed 's/\\//g' | sed 's/""//g' | sed 's/","/:/g' | sed "s/^/anchore-cli image add $ecrid.dkr.ecr.$region.amazonaws.com\//" | sh -i
+    aws ecr describe-images --profile=$profile --repository-name $repo --output json \
+        | jq '.imageDetails[] | [.repositoryName, .imageTags[]] | @csv' | sed 's/"\"//g' \
+        | sed 's/\\//g' | sed 's/""//g' | sed 's/","/:/g' \
+        | sed "s/^/anchore-cli image add $ecrid.dkr.ecr.$region.amazonaws.com\//" | sh -i
 done
 
 
