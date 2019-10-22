b256c6f573cb1398a63b92feb61168a80d947074
Cem Bas <ceb@netic.dk>
2019-10-22 10:18:49 +0200

    Added region

--- ecr-anchore-add.sh
+++ ecr-anchore-add.sh
@@ -4,17 +4,16 @@
 # Run on chron or in a pipeline to keep Anchore up to date and monitoring all new images as they're added to ECR
 
 # Set variables
-region=$0
 ecrid=$1
 
 repolist=$(
-    aws ecr describe-repositories | grep repositoryName \
+    aws ecr describe-repositories --region eu-west-1 | grep repositoryName \
         | awk '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' \
-        | awk '{ print "aws ecr describe-images --repository-name " $1 }' | sed 's/.* //g')
+        | awk '{ print "aws ecr describe-images --region eu-west-1 --repository-name " $1 }' | sed 's/.* //g')
 
 for repo in $repolist
 do
-    aws ecr describe-images --repository-name $repo --output json \
+    aws ecr describe-images --repository-name $repo --output json --region eu-west-1 \
         | jq '.imageDetails[] | [.repositoryName, .imageTags[]] | @csv' | sed 's/"\"//g' \
         | sed 's/\\//g' | sed 's/""//g' | sed 's/","/:/g' \
         | sed "s/^/anchore-cli image add $ecrid.dkr.ecr.$region.amazonaws.com\//" | sh -i
