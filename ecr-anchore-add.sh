0e0f50406b6d88cb2a1ebfdd7dcd9e541657c7af
Cem Bas <ceb@netic.dk>
2019-10-22 10:28:50 +0200

    Made it run

--- ecr-anchore-add.sh
+++ ecr-anchore-add.sh
@@ -9,13 +9,13 @@ region=eu-west-1
 ecrid=$1
 
 repolist=$(
-    aws ecr describe-repositories | grep repositoryName \
+    aws ecr describe-repositories --region eu-west-1 | grep repositoryName \
         | awk '{ print $2 }' | sed 's/\"//g' | sed 's/\,//g' \
         | awk '{ print "aws ecr describe-images --repository-name " $1 }' | sed 's/.* //g')
 
 for repo in $repolist
 do
-    aws ecr describe-images --repository-name $repo --output json \
+    aws ecr describe-images --repository-name $repo --output json --region eu-west-1 \
         | jq '.imageDetails[] | [.repositoryName, .imageTags[]] | @csv' | sed 's/"\"//g' \
         | sed 's/\\//g' | sed 's/""//g' | sed 's/","/:/g' \
         | sed "s/^/anchore-cli image add $ecrid.dkr.ecr.$region.amazonaws.com\//" | sh -i
